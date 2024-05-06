d01b9bc0e058e3f02eae8a175469ca538716ae7a
unknown <yedhu8566@gmail.com>
2024-05-06 19:40:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './homePage/HomePage';
+import ProjectView from './homePage/ProjectView';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/projects/:projectId" component={ProjectView} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
